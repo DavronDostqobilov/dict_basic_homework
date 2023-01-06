75f7fd4eeaf10f43803d940a34d99fef558f8858
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-01-06 13:05:36 +0500

    count_all

--- count_all.py
+++ count_all.py
@@ -16,6 +16,6 @@ def count_all(txt):
         if txt[i].isdigit():
             k+=1
         i+=1
-    dict1={'latters': j,'digits': k}
+    dict1={'LETTERS': j,'DIGITS': k}
     return dict1
 print(count_all('davr1on boy'))
